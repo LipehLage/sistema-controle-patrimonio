426874ef230afd225770e041ff5cfe836c58b37b
Felipe.Lage <felipe.lage@fourlog.com>
2025-06-25 14:46:58 -0300

    deletando arquivos duplicados

--- asset-control-system-backend/src/main/java/com/eof/asset_control_system_backend/MovementRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eof.asset_control_system_backend;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MovementRepository extends JpaRepository<Movement, Long> {
-
-}
--- asset-control-system-backend/src/main/java/com/eof/asset_control_system_backend/MovementRequestPayload.java
+++
@@ -1,9 +0,0 @@
-package com.eof.asset_control_system_backend;
-
-public record MovementRequestPayload(
-        Long equipmentId,
-        Long appUserId,
-        String movementType,
-        String reason
-) {
-}
